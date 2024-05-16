9c9a80ecb97ec7232278bd9100a38677df9ae00c
kiranazka <160616395+kiranazka@users.noreply.github.com>
2024-05-16 12:35:57 -0700

    Anonymous functions
    Assign a function expression to a variable, with one parameter that outputs the provided 
    argument to the console.
    • Pass an argument into the function.
    • Create the same function as a normal function declaration

---
+++ main.js
@@ -0,0 +1,5 @@
+var myFunction = function (arg) {
+    console.log(arg);
+};
+// calling the function
+myFunction("Helo,World");
---
+++ main.ts
@@ -0,0 +1,7 @@
+const myFunction = function(arg: any){
+    console.log(arg);
+};
+// calling the function
+myFunction("Helo,World");
+
+
